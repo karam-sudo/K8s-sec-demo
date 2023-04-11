0706c763c70c59fc3f0ad1406a15af8637f69fe4
karam-sudo <karamalhalabi1994@outlook.com>
2023-04-11 05:02:28 -0400

    Unit Test

--- setup/vm-install-script/install-script.sh
+++ setup/vm-install-script/install-script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo ".........----------------#################._.-.-INSTALL-.-._.#################----------------........."
-PS1='\[\e[01;36m\]\u\[\e[01;37m\]@\[\e[01;33m\]\H\[\e[01;37m\]:\[\e[01;32m\]\w\[\e[01;37m\]\$\[\033[0;37m\] '
-echo "PS1='\[\e[01;36m\]\u\[\e[01;37m\]@\[\e[01;33m\]\H\[\e[01;37m\]:\[\e[01;32m\]\w\[\e[01;37m\]\$\[\033[0;37m\] '" >> ~/.bashrc
-sed -i '1s/^/force_color_prompt=yes\n/' ~/.bashrc
-source ~/.bashrc
+#PS1='\[\e[01;36m\]\u\[\e[01;37m\]@\[\e[01;33m\]\H\[\e[01;37m\]:\[\e[01;32m\]\w\[\e[01;37m\]\$\[\033[0;37m\] '
+#echo "PS1='\[\e[01;36m\]\u\[\e[01;37m\]@\[\e[01;33m\]\H\[\e[01;37m\]:\[\e[01;32m\]\w\[\e[01;37m\]\$\[\033[0;37m\] '" >> ~/.bashrc
+#sed -i '1s/^/force_color_prompt=yes\n/' ~/.bashrc
+#source ~/.bashrc
 
 apt-get autoremove -y  #removes the packages that are no longer needed
 apt-get update
